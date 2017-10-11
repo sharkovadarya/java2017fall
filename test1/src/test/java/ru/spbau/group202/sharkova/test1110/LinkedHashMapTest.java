072ed2e10d51885ecfaf6b065e67a591bcdc7ddb
sharkovadarya <sharkovadarya@gmail.com>
2017-10-11 18:20:21 +0300

    Added a test and fixed another test

--- test1/src/test/java/ru/spbau/group202/sharkova/test1110/LinkedHashMapTest.java
+++ test1/src/test/java/ru/spbau/group202/sharkova/test1110/LinkedHashMapTest.java
@@ -21,8 +21,9 @@ public class LinkedHashMapTest {
         LinkedHashMap<String, Integer> ht = new LinkedHashMap<>();
 
         assertEquals(null, ht.put("test1", 30));
+        assertEquals(null, ht.put("test2", 30));
         assertEquals(30, ht.remove("test1"));
-        assertEquals(false, ht.containsValue(30));
+        assertEquals(true, ht.containsValue(30));
         assertEquals(false, ht.containsKey("test1"));
     }
 
@@ -36,6 +37,14 @@ public class LinkedHashMapTest {
         assertEquals(false, ht.containsKey("test"));
         assertEquals(true, ht.containsValue(30));
         assertEquals(false, ht.containsValue(29));
+    }
+
+    @Test
+    public void testReplace() {
+        LinkedHashMap<String, Integer> ht = new LinkedHashMap<>();
+
+        assertEquals(null, ht.put("test1", 30));
+        assertEquals(30, ht.put("test1", 59));
     }
 
 }
