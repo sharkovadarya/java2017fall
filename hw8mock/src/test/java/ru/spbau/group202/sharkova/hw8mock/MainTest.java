e9eea3f3deac70263651e800046d3871d8b570b0
sharkovadarya <sharkovadarya@gmail.com>
2017-11-29 14:20:16 +0300

    Added javadoc comments to MainTest

--- hw8mock/src/test/java/ru/spbau/group202/sharkova/hw8mock/MainTest.java
+++ hw8mock/src/test/java/ru/spbau/group202/sharkova/hw8mock/MainTest.java
@@ -7,18 +7,36 @@ import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
+/**
+ * This class tests correctness of Main class behavior, 
+ * which is parsing a given expression and calculating its result
+ * using reverse Polish notation and a stack.
+ */
 public class MainTest {
 
+    /**
+     * This method checks that expression consisting of a single number
+     * is considered incorrect and causes an IllegalArgumentException to be thrown.
+     */
     @Test (expected = IllegalArgumentException.class)
     public void testIncorrectExpressionSingleNumber() {
         Main.main(new String[]{"1234"});
     }
 
+    /**
+     * This method checks that expression consisting of a single number and an operation
+     * is considered incorrect and causes an IllegalArgumentException to be thrown.
+     */
     @Test (expected = IllegalArgumentException.class)
     public void testIncorrectExpressionSingleOperation() {
         Main.main(new String[]{"1234 +"});
     }
 
+    /**
+     * This method checks correctness of calculation
+     * of a basic arithmetic expression with on operation
+     * and its two arguments.
+     */
     @Test
     public void testBasicExpression() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -35,6 +53,9 @@ public class MainTest {
         assertEquals("46.0", baos.toString().trim());
     }
 
+    /**
+     * This method checks correctness of simple expression with brackets calculation.
+     */
     @Test
     public void testExpressionWithBrackets() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -46,11 +67,14 @@ public class MainTest {
 
         System.out.flush();
         System.setOut(old);
-
-
+        
         assertEquals("178.0", baos.toString().trim());
     }
 
+    /**
+     * This method checks correctness of calculations of an expression
+     * which uses brackets, double numbers and all supported operations.
+     */
     @Test
     public void testExpressionAllOperations() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -66,6 +90,11 @@ public class MainTest {
         assertEquals(Double.parseDouble("15.55"), Double.parseDouble(baos.toString().trim()), 0.01);
     }
 
+    /**
+     * This method checks if an IllegalArgumentException is indeed thrown
+     * when there's an attempt to calculate the result of an expression
+     * without the proper spacing.
+     */
     @Test (expected = IllegalArgumentException.class)
     public void testNoNecessarySpaces() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -81,6 +110,11 @@ public class MainTest {
         assertEquals("Invalid input. Separate all operations with spaces.", baos.toString().trim());
     }
 
+    /**
+     * This method checks if an IllegalArgumentException is indeed thrown
+     * when there's an attempt to calculate the result of an expression
+     * with an unsupported operation.
+     */
     @Test (expected = IllegalArgumentException.class)
     public void testIncorrectOperation() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
